0aba2deba576e26f622501e463c0b328a8987475
Robin Pahlman <robin.pahlman@gmail.com>
2023-11-20 12:21:37 -0500

    styling edits

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+        'albert-sans': ['"Albert Sans"', 'sans-serif'],
+				'barlow': ['"Barlow"', 'sans-serif'],
+      },
+		},
 	},
 	plugins: [],
 }
